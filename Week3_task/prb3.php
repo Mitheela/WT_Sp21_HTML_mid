c9ac633126b0cae17a7277ac786c3f85fc57bed4
Mitheela <57425136+Mitheela@users.noreply.github.com>
2021-02-07 16:08:22 +0600

    Add files via upload

---
+++ Week3_task/prb3.php
@@ -0,0 +1,10 @@
+<?php
+$a=7;
+$b=7.5;
+if($a==$b) echo "This shape is a square.";
+else{
+$area=$a*$b;
+echo "Area is:".$area;
+$perimeter=2*($a+$b);
+echo ", Perimeter is:".$perimeter;}
+?>
---
+++ Week3_task/prb4.php
@@ -0,0 +1,11 @@
+<?php
+$name="raju";
+if($name=="raju"||$name=="mina"||$name=="mithu") 
+{echo "
+<!DOCTYPE html>
+<html> <body> <b>
+hello
+</b> </body> </html>
+";}
+else {echo "you are not recognized.";}
+?>
